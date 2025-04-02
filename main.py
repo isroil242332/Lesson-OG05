d748944bbfe39308bb165f5643cb25e9b05199c7
isroil242332 <isroil-islam@mail.ru>
2025-04-02 20:21:54 +0300

    add base code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 
 pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра тир')
+icon = pygame.image.load('assets/image/gunman.jpeg')
+pygame.display.set_icon(icon)
+
+
 run = True
 while run:
     pass
